9e1ebd8c4561905c8f2eb4c34e1dfc14b4c4e546
NCasqueira37 <nickcas37@gmail.com>
2026-02-16 01:33:46 -0500

    Server -Error handling for creating the listener - function to get address from user

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,21 +1,41 @@
-use std::thread;
+use std::{io, option, thread};
 use std::{
     io::Read,
     net::{TcpListener, TcpStream},
 };
 
 fn main() {
-    create_listener(String::from("127.0.0.1:80"));
+    create_listener();
 }
 
-fn create_listener(addr: String) {
-    let listener = TcpListener::bind(addr).unwrap();
+fn get_addr() -> String {
+    println!("Server Address:");
+    let mut buf = String::new();
+    let _ = io::stdin().read_line(&mut buf);
+    buf.trim().to_string()
+}
+
+fn create_listener() {
+    let listener = loop{
+        let addr = get_addr();
+        let listener = TcpListener::bind(&addr);
+        match listener {
+            Ok(listener) => break listener,
+            Err(_e) => {
+                println!("Address not valid, try again.");
+                get_addr();
+            },
+        }
+    };
+    
+    // Show address in console
+    println!("Server started on: {:?}", listener.local_addr().unwrap());
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
+                // Handle each stream in a different thread
                 thread::spawn(move || {
-                    // Handle each stream in a different thread
                     handle_stream(stream);
                 });
             }
