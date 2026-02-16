ffd6dac738ff1f2b86d6fb1102ee0fa7ac6825b3
NCasqueira37 <nickcas37@gmail.com>
2026-02-16 00:25:55 -0500

    Server can now read multiple messages from 1 user Fixed stream going out of scope and being dropped Server loops through each stream to see if more messaged were being sent

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,16 +1,33 @@
-use std::net::TcpStream;
+use std::io;
 use std::io::Write;
+use std::net::TcpStream;
 
 fn main() {
     let addr = String::from("127.0.0.1:80");
-    let stream = TcpStream::connect(addr);
-    match stream {
-        Ok(mut stream) => {
-            let buf = b"hello world";
-            stream.write(buf).unwrap();
+    let mut stream = TcpStream::connect(addr);
+    let mut buf: String = String::from("");
+
+    loop {
+        // Clear buffer
+        buf.clear();
+        match io::stdin().read_line(&mut buf) {
+            Ok(_n) => {
+                println!("Message: {}", buf);
+            }
+            Err(err) => {
+                println!("{}", err.kind());
+            }
         }
-        Err(e) => {
-            println!("{}", e);
+
+        match &mut stream {
+            Ok(stream) => {
+                stream.write_all(buf.as_bytes()).unwrap_or_else(|err| {
+                    println!("{}", err.kind());
+                });
+            }
+            Err(e) => {
+                println!("{}", e.kind());
+            }
         }
     }
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,33 +1,50 @@
-use std::{io::Read, net::{TcpListener, TcpStream}};
 use std::thread;
+use std::{
+    io::Read,
+    net::{TcpListener, TcpStream},
+};
 
-fn main(){
+fn main() {
     create_listener(String::from("127.0.0.1:80"));
 }
 
 pub fn create_listener(addr: String) {
     let listener = TcpListener::bind(addr).unwrap();
-    
-    loop {
-        for stream in listener.incoming() {
-            match stream {
-                Ok(stream) => {
-                    thread::spawn(|| {
-                        // Handle each stream in a different thread
-                        handle_stream(stream);
-                    });
-                }
-                Err(e) => {
-                    println!("ERROR: {e}");
-                }
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move || {
+                    // Handle each stream in a different thread
+                    handle_stream(stream);
+                });
+            }
+            Err(e) => {
+                println!("ERROR: {e}");
             }
         }
     }
-    
 }
 
 fn handle_stream(mut stream: TcpStream) {
     let mut buffer = [0u8; 1024];
-    let bytes_read = stream.read(&mut buffer);
-    println!("{:?}: {}", stream.local_addr(), String::from_utf8_lossy(&buffer[..bytes_read.unwrap()]));
+    loop {
+        buffer.fill(0u8);
+        let bytes_read = stream.read(&mut buffer);
+        match bytes_read {
+            Ok(r) => {
+                // Print data from user to console
+                println!(
+                    "{:?}(size: {}): {}",
+                    stream.local_addr(),
+                    r,
+                    String::from_utf8_lossy(&buffer[..bytes_read.unwrap()])
+                );
+            }
+            Err(e) => {
+                println!("{:?} DISCONNECTED ({})", stream.local_addr(), e);
+                break;
+            }
+        }
+    }
 }
